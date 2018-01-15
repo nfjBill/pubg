2398bdf310c87765ead809c2fdb37fc9b6c78070
nfjbill <nfjbill@qq.com>
2018-01-15 09:47:58 +0800

    update
    update

--- react-app-rewire-style-lint/index.js
+++ react-app-rewire-style-lint/index.js
@@ -1,6 +1,10 @@
 const StyleLintPlugin = require('stylelint-webpack-plugin');
+const config = require('./stylelint-config');
 
-const createRewireStyleLint = function (options = {files: '**/*.+(c|sa|sc|le)ss'}) {
+const createRewireStyleLint = function (options = {
+  files: 'src/**/*.+(c|sa|sc|le)ss',
+  config,
+}) {
   return function (config, env) {
     config.plugins.push(new StyleLintPlugin(options));
 
